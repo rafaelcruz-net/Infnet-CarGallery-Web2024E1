e1d21fe2911010ab12eaeb436ab55ef1c47539b6
rafael.cruz <rafael.cruz@SJ207-99>
2024-03-08 19:43:39 -0300

    Adicionando upload de arquivos

--- CarGallery/Controllers/CarroController.cs
+++ CarGallery/Controllers/CarroController.cs
@@ -30,6 +30,30 @@ namespace CarGallery.Controllers
             if (fabricante == null)
                 throw new Exception("Fabricante não encontrado");
 
+            var image = Request.Form.Files.GetFile("imageFile");
+
+            if (image != null && image.ContentType.StartsWith("image/") == false)
+            {
+                ModelState.AddModelError("image_error", "Extensão de arquivo não permitada");
+
+                var fabricantes = _context.Fabricantes.ToList();
+                ViewBag.fabricantes = fabricantes;
+
+                return View();
+
+            }
+
+            var fileName = image.FileName;
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "imagens_carros", image.FileName);
+
+            using (System.IO.Stream stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+                stream.Flush();
+            }
+
+            carro.Imagem = $"/imagens_carros/{image.FileName}";
+
             fabricante.Carros.Add(carro);
            
             this._context.Fabricantes.Update(fabricante);
